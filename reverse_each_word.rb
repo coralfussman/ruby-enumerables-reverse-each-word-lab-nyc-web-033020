602e2eff8a06b8741d9d9884c553c3b0e73ed928
Coral Fussman <coralfussman@gmail.com>
2020-03-24 00:50:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,11 @@
 def reverse_each_word("Hello there, and how are you?")
 
-reverse_each_array = 
+string ="Hello there, and how are you?"
+reverse_each_array = []
+index = 0
+reverse_each_array.push(string)
+reverse_each_array.reverse
+
+reverse_each_array
+
 end
